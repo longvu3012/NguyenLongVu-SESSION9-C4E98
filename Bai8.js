73faaf39242101e94b68e499e2b31d7d5a1b389f
longvu3012 <72186778+longvu3012@users.noreply.github.com>
2020-10-30 13:40:17 +0700

    Create Bai8.js

---
+++ Bai8.js
@@ -0,0 +1,21 @@
+
+// Bai 8:
+// function My(Vu , money){
+//     console.log("My name is Vu, i wish i have money");
+// }
+
+
+// Bai 9:
+// function bai9(name, wish){
+//     alert(`My name is ${name}, i wish ${wish}`);
+// bai9("Vu", "money");
+// }
+
+// Bai10: 
+function bai10(name, wish){
+    if(wish == undefined){
+        console.log("Bạn không có điều ước nào cả");
+    }else{
+        alert(`My name is ${name}, i wish ${wish}`);
+    }
+}
