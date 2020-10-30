139fcd80a23357d8bb2001a8d617e4aaf6e4250f
longvu3012 <72186778+longvu3012@users.noreply.github.com>
2020-10-30 13:39:31 +0700

    Create Bai7.js

---
+++ Bai7.js
@@ -0,0 +1,8 @@
+var btn1 = document.getElementById("btn-1");
+var btn2 = document.getElementById("btn-2");
+
+// event.target sẽ in ra 2 element là <button id="btn-1">Click 1</button> <button id="btn-2"> Click 2</button>
+btn1.addEventListener(`keydown`, function (){
+    console.log("");
+}
+// e.key in ra mọi thứ vừa nhâp
